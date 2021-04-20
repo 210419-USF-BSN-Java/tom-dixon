ad8ef0dbd5e92b85bb8032e67614b288dd04d751
tom <thomcdixon@gmail.com>
2021-04-20 12:49:37 -0700

    add first assignment

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+
+public class App {
+	
+	public static void main(String[] args) {
+		Assignment1 a1 = new Assignment1();
+		
+		System.out.println(a1.reverse("hello"));
+	}
+
+}
---
+++ src/main/java/Assignment1.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Assignment1 {
+
+		public String printNumberInWord(int number) {
+				// TODO Write an implementation for this method declaration
+			String result;
+			switch(number) {
+			case(0):
+				result = "ZERO";
+				break;
+			case(1):
+				result = "ONE";
+				break;
+			case(2):
+				result = "TWO";
+				break;
+			case(3):
+				result = "THREE";
+				break;
+			case(4):
+				result = "FOUR";
+				break;		
+			case(5):
+				result = "FIVE";
+				break;
+			case(6):
+				result = "SIX";
+				break;
+			case(7):
+				result = "SEVEN";
+				break;
+			case(8):
+				result = "EIGHT";
+				break;
+			case(9):
+				result = "NINE";
+				break;
+			default:
+				result = "OTHER";
+			
+			}
+				
+				return result;
+}
+
+		public String reverse(String string) {
+			String reversed = "";
+				// Split string
+				String[] strArr = string.split("");
+				// Reverse
+				Collections.reverse(Arrays.asList(strArr));
+				
+				// Join
+				for(String s: strArr) {
+					reversed.concat(s);
+				}
+				return reversed;
+		}
+
+}
+
+
