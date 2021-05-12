dc2ecb9218284ab1b97d91201213f8dab04414c1
tom <thomcdixon@gmail.com>
2021-05-11 20:24:32 -0700

    implement get user by User and get user by username in repo layer

--- project1/ers/src/main/java/repository/daos/UserDao.java
+++ project1/ers/src/main/java/repository/daos/UserDao.java
@@ -7,13 +7,15 @@ import repository.models.User;
 public interface UserDao {
 
     // crud operations
-    User add(User u);
+    int add(User u);
 
     List<User> getAll();
 
     User get(User i);
 
-    int update(User i);
+    User getByUsername(String username);
+
+    User update(User i);
 
     int remove(User i);
 
