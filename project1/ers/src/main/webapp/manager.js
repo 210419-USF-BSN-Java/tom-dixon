1478f427ff92bff206031b1c1f1c8f0a1108a73b
tom <thomcdixon@gmail.com>
2021-05-14 14:31:44 -0700

    add loading spinner

--- project1/ers/src/main/webapp/manager.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener( 'load', ( event ) => {
-    // remove spinner from dom after 2.5 seconds
-
-
-} );
-
---
+++ project1/ers/src/main/webapp/scripts/page-load.js
@@ -0,0 +1,17 @@
+window.addEventListener( 'load', ( event ) => {
+    setTimeout( function () {
+        // remove spinner from DOM
+        console.log( "IS THIS THING ON" )
+        document.querySelector( ".spinner-container" ).remove();
+        // get 
+
+    }, 2000 );
+
+
+
+} );
+
+
+
+
+
