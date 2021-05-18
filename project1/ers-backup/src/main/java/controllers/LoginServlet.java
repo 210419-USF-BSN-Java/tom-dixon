326c29326d6d8224bb2b78295302088aa6cb14e3
tom <thomcdixon@gmail.com>
2021-05-18 16:08:47 -0700

    nuke project

---
+++ project1/ers-backup/src/main/java/controllers/LoginServlet.java
@@ -0,0 +1,55 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import repository.models.User;
+import services.UserService;
+
+public class LoginServlet extends HttpServlet {
+
+    private UserService uService = new UserService();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+        // try get parameter
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        User user = uService.login(username, password);
+        // if valid,
+        if (user != null) {
+            if (user.getRoleId() == 1) {
+                System.out.println("REROUTING TO MANAGER HOME");
+                System.out.println("PLLOOOMMMMPPP");
+                request.getRequestDispatcher("manager.html").forward(request, response);
+            } else {
+                System.out.println("REROUTING TO EMPLOYEE HOME");
+                System.out.println("PLLOOOMMMMPPP");
+                request.getRequestDispatcher("employee.html").forward(request, response);
+            }
+        } else {
+            System.out.println("SOMETHING WENT WRONG");
+        }
+
+    };
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        System.out.println("HELLLLOOOOOOO");
+    }
+
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+    }
+}
