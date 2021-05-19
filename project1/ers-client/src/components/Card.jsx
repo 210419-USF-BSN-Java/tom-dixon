63243f520035020eaaa5193ffb3528081449ec00
tom <thomcdixon@gmail.com>
2021-05-19 06:53:58 -0700

    extract card icon  svg as jsx

--- project1/ers-client/src/components/Card.jsx
+++ project1/ers-client/src/components/Card.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ icon }) => {
   return (
     <div class='menu-card' name='cust-request-new' id='custRequestNew'>
       <div class='card-header card-subheader-container'>
         <h3 class='fs-6 tertiary-color flex-baseline'>
-          <svg
+          {icon()}
+          {/* <svg
             class='mr-sm'
             height='16'
             width='16'
@@ -16,7 +17,7 @@ const Card = () => {
               d='M12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zM3 1h8a1 1 0 0 1 0 2H3a1 1 0 1 1 0-2zm6.93 5.366A5.98 5.98 0 0 0 8.683 7H1a1 1 0 1 1 0-2h8a1 1 0 0 1 .93 1.366zM6.804 9a5.96 5.96 0 0 0-.72 2H2a1 1 0 0 1 0-2zm-.72 4a5.96 5.96 0 0 0 .72 2H1a1 1 0 0 1 0-2z'
               fill-rule='evenodd'
             ></path>
-          </svg>
+          </svg> */}
           Request
         </h3>
         <p>Submit a new request for reimbursement</p>
--- project1/ers-client/src/components/pages/Main.jsx
+++ project1/ers-client/src/components/pages/Main.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import Page from '../Page';
 import Card from '../Card';
+import fatStacks from '../../svg-icons/fatStacks';
 
 const Main = ({ user }) => {
   //TODO get reimbursements according to user role/id
   //TODO add logout when user state
+
   return (
     <Page>
       <h1 style={{ fontSize: 'calc(1.3rem + .6vw)' }}>Reimbursements</h1>
       <h3 class='text-xl'>{user.roleId == 1 ? 'Manager' : 'Employee'}</h3>
       <div name='card-container' class=''>
-        <Card />
-        <Card />
-        <Card />
+        <Card icon={fatStacks} />
+        {/* <Card />
+        <Card /> */}
       </div>
     </Page>
   );
