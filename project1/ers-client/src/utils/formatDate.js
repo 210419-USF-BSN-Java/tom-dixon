8ed71e1211213ad28f54c3bfdb6406631bce675a
tom <thomcdixon@gmail.com>
2021-05-23 10:06:59 -0700

    update formatDate util to save table space

--- project1/ers-client/src/utils/formatDate.js
+++ project1/ers-client/src/utils/formatDate.js
@@ -1,9 +1,11 @@
 function formatDate( date ) {
     const now = new Date( date );
-    const inter = now.toDateString().split( ' ' ).slice( 1 ).join( ' ' );
-
-    console.log( inter )
-    return now.toDateString().split( ' ' ).slice( 1 ).join( ' ' );
+    // const month = now.getMonth() + 1
+    // const day = now.getDate();
+    // const year = now.getFullYear()
+    // // const inter = now.toDateString().split( ' ' ).slice( 1 ).join( ' ' );
+    // console.log( `${ now.getMonth() + 1 }/${ now.getDate() }/${ now.getFullYear().toString().slice( 2 ) }` )
+    return `${ now.getMonth() + 1 }/${ now.getDate() }/${ now.getFullYear().toString().slice( 2 ) }`
 }
 
 export default formatDate
