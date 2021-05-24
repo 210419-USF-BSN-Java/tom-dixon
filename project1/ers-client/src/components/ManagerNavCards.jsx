de3ebe5dded0f2f1b02a2d4483c9f6e6368beed1
tom <thomcdixon@gmail.com>
2021-05-23 23:10:30 -0700

    code cleanup

--- project1/ers-client/src/components/ManagerNavCards.jsx
+++ project1/ers-client/src/components/ManagerNavCards.jsx
@@ -1,5 +1,4 @@
 import Card from './Card';
-import userIcon from '../svg-icons/userIcon';
 import usersIcon from '../svg-icons/usersIcon';
 import time from '../svg-icons/time';
 import bank from '../svg-icons/bank';
@@ -20,12 +19,6 @@ const managerNavCards = ({ allReqs, allResolved, allEmployees }) => {
           title='Resolved'
           desc='View all approved/rejected reimbursement requests'
         />
-        {/* <Card
-          action={allReqs}
-          icon={userIcon}
-          title='Employee requests'
-          desc='View request history of a single employee'
-        /> */}
         <Card
           action={allEmployees}
           icon={usersIcon}
--- project1/ers-client/src/components/pages/LoginPage.jsx
+++ project1/ers-client/src/components/pages/LoginPage.jsx
@@ -22,22 +22,13 @@ const LoginPage = ({ login, user }) => {
         enter='transition-opacity duration-1000'
         enterTo='opacity-100'
         enterFrom='opacity-0'
-        leave='transition-opacity duration-150'
-        leaveFrom='opacity-0'
-        leaveTo='opacity-100'
       >
         <Transition.Child
-          enter='transition ease-in-out duration-500 transform'
+          enter='transition ease-in-out duration-300 transform'
           enterFrom='translate-y-full'
           enterTo='translate-y-0'
-          leave='transition ease-in-out duration-300 transform'
-          leaveFrom='translate-x-0'
-          leaveTo='-translate-x-full'
         >
-          <div
-            className={`flex flex-col text-gray-800 justify-center  pt-36 pb-10
-        }`}
-          >
+          <div className='flex flex-col text-gray-800 justify-center  pt-36 pb-10'>
             <h2 className='mx-auto text-6xl font-mono tracking-widest text-gray-700	title-shadow'>
               ERS
             </h2>
@@ -51,17 +42,11 @@ const LoginPage = ({ login, user }) => {
         enter='transition-opacity duration-1000'
         enterTo='opacity-100'
         enterFrom='opacity-0'
-        leave='transition-opacity duration-150'
-        leaveFrom='opacity-0'
-        leaveTo='opacity-100'
       >
         <Transition.Child
           enter='transition ease-in-out duration-500 transform'
           enterFrom='translate-y-full'
           enterTo='translate-y-0'
-          leave='transition ease-in-out duration-300 transform'
-          leaveFrom='translate-x-0'
-          leaveTo='-translate-x-full'
         >
           <LoginForm login={login} user={user} />
         </Transition.Child>
